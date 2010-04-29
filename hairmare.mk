7f48b87bc30f33a761af4f1ceb6677ff81044da4
Lucas Bickel <hairmare@purplehaze.ch>
2010-04-29 20:02:46 +0200

    documentation w/ simple website
    this cleans up all this website related for now, im happy with
    publishing this way for the time beaing, ill tweak this to support
    uploading of images and whatnot when they are ready for prime time.

--- hairmare.mk
+++ hairmare.mk
@@ -27,11 +27,13 @@ repo/openwrt/update:
 
 repo/patch: feeds/install repo/openwrt/patch repo/packages/patch
 
-repo/openwrt/patch: patches/openwrt.patch
-	cd ../openwrt && git apply ../mjb/patches/openwrt.patch
+repo/openwrt/patch: patches/openwrt.patch 
+	cd ../openwrt && if [[ ! -f .mjb.patched ]]; then git apply ../mjb/patches/openwrt.patch; fi
+	touch ../openwrt/.mjb.patched
 
-repo/packages/patch: patches/openwrt_packages.patch
-	cd ../openwrt/feeds/packages/  && git apply ../../../mjb/patches/openwrt_packages.patch
+repo/packages/patch: patches/openwrt_packages.patch 
+	cd ../openwrt/feeds/packages/  && if [[ ! -f .mjb.patched ]]; then git apply ../../../mjb/patches/openwrt_packages.patch; fi
+	touch ../openwrt/feeds/packages/.mjb.patched
 	
 repo/world: feeds/install repo/patch repo/openwrt/install
 	cd ../openwrt && make world
@@ -47,16 +49,23 @@ package/dropbear/install:
 	cp keys/default.rsa.pub ../openwrt/package/dropbear/files/dropbear.authorized_keys
 
 
-distro/upload: distro/docs
-	rsync -az --delete --force _site/ hairmare@hairmare.ch:/var/www/mjb.hairmare.ch/htdocs/
+distro/upload: distro/site
+	rsync -az --delete --force _site/ hairmare.ch:/var/www/mjb.hairmare.ch/htdocs/
+
+distro/site:distro/docs distro/page
 	
-distro/docs: _site/docs _site/docs/index.html
+distro/page: html/index.textile
+	cp html/*.js _site/
+	cat html/header.html > _site/index.html
+	redcloth html/index.textile >> _site/index.html
+	cat html/footer.html >> _site/index.html
+
+distro/docs: _site/docs _site/docs/index.html 
 
 _site/docs:
 	mkdir -p _site/docs
 
 _site/docs/index.html: html/*.js html/header.html html/footer.html HOWTO
-	cp html/*.js _site/docs/
 	cat html/header.html > _site/docs/index.html
 	redcloth HOWTO >> _site/docs/index.html
 	cat html/footer.html >> _site/docs/index.html
